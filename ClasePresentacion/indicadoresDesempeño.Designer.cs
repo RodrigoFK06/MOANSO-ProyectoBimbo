7353af496620fc80e5e198078ad5f07bcb236c36
Michael <michaelcristopher21@gmail.com>
2023-10-23 21:33:31 -0500

    Interface de indicadores de Desempeño

---
+++ ClasePresentacion/indicadoresDesempeño.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace ClasePresentacion
+{
+    partial class indicadoresDesempeño
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxEmpleado = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombresEmpleado = new System.Windows.Forms.TextBox();
+            this.txtApellidosEmpleados = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBoxDesempeno = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBoxEmpleado.SuspendLayout();
+            this.groupBoxDesempeno.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxEmpleado
+            // 
+            this.groupBoxEmpleado.Controls.Add(this.textBox2);
+            this.groupBoxEmpleado.Controls.Add(this.textBox1);
+            this.groupBoxEmpleado.Controls.Add(this.comboBox2);
+            this.groupBoxEmpleado.Controls.Add(this.comboBox1);
+            this.groupBoxEmpleado.Controls.Add(this.label4);
+            this.groupBoxEmpleado.Controls.Add(this.label6);
+            this.groupBoxEmpleado.Controls.Add(this.label5);
+            this.groupBoxEmpleado.Controls.Add(this.label3);
+            this.groupBoxEmpleado.Controls.Add(this.txtApellidosEmpleados);
+            this.groupBoxEmpleado.Controls.Add(this.txtNombresEmpleado);
+            this.groupBoxEmpleado.Controls.Add(this.label2);
+            this.groupBoxEmpleado.Controls.Add(this.label1);
+            this.groupBoxEmpleado.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxEmpleado.Name = "groupBoxEmpleado";
+            this.groupBoxEmpleado.Size = new System.Drawing.Size(356, 297);
+            this.groupBoxEmpleado.TabIndex = 0;
+            this.groupBoxEmpleado.TabStop = false;
+            this.groupBoxEmpleado.Text = "Información Empleado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre(s):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellidos:";
+            // 
+            // txtNombresEmpleado
+            // 
+            this.txtNombresEmpleado.Location = new System.Drawing.Point(120, 48);
+            this.txtNombresEmpleado.Name = "txtNombresEmpleado";
+            this.txtNombresEmpleado.Size = new System.Drawing.Size(198, 20);
+            this.txtNombresEmpleado.TabIndex = 2;
+            // 
+            // txtApellidosEmpleados
+            // 
+            this.txtApellidosEmpleados.Location = new System.Drawing.Point(120, 82);
+            this.txtApellidosEmpleados.Name = "txtApellidosEmpleados";
+            this.txtApellidosEmpleados.Size = new System.Drawing.Size(198, 20);
+            this.txtApellidosEmpleados.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Departamento:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Dirección:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Teléfono:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cargo:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(120, 196);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(120, 234);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(198, 21);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(198, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // groupBoxDesempeno
+            // 
+            this.groupBoxDesempeno.Controls.Add(this.label10);
+            this.groupBoxDesempeno.Controls.Add(this.comboBox3);
+            this.groupBoxDesempeno.Controls.Add(this.button1);
+            this.groupBoxDesempeno.Controls.Add(this.listView1);
+            this.groupBoxDesempeno.Controls.Add(this.numericUpDown1);
+            this.groupBoxDesempeno.Controls.Add(this.dateTimePicker1);
+            this.groupBoxDesempeno.Controls.Add(this.textBox3);
+            this.groupBoxDesempeno.Controls.Add(this.label9);
+            this.groupBoxDesempeno.Controls.Add(this.label8);
+            this.groupBoxDesempeno.Controls.Add(this.label7);
+            this.groupBoxDesempeno.Location = new System.Drawing.Point(411, 12);
+            this.groupBoxDesempeno.Name = "groupBoxDesempeno";
+            this.groupBoxDesempeno.Size = new System.Drawing.Size(364, 426);
+            this.groupBoxDesempeno.TabIndex = 1;
+            this.groupBoxDesempeno.TabStop = false;
+            this.groupBoxDesempeno.Text = "Indicadores de Desempeño";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(19, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 15);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Fecha de registro:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(19, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 15);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Calificación:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(19, 125);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 15);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Comentarios:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(22, 157);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(323, 98);
+            this.textBox3.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(148, 48);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(148, 87);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(182, 293);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(163, 127);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Agregar Indicador";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Productivo",
+            "Eficiente",
+            "Asistencia y puntualidad",
+            "Capacidad de Liderazgo y/o Trabajo en equipo",
+            "Innovador"});
+            this.comboBox3.Location = new System.Drawing.Point(22, 295);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(135, 21);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(19, 269);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(187, 15);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Indicadores de Desempeño:";
+            // 
+            // indicadoresDesempeño
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxDesempeno);
+            this.Controls.Add(this.groupBoxEmpleado);
+            this.Name = "indicadoresDesempeño";
+            this.Text = "indicadoresDesempeño";
+            this.groupBoxEmpleado.ResumeLayout(false);
+            this.groupBoxEmpleado.PerformLayout();
+            this.groupBoxDesempeno.ResumeLayout(false);
+            this.groupBoxDesempeno.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxEmpleado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtApellidosEmpleados;
+        private System.Windows.Forms.TextBox txtNombresEmpleado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBoxDesempeno;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
