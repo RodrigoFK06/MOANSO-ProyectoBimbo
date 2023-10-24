bff4365af77e51a73f2fd1cd86e94afe9f3cbed4
Rodrigo <clanprotm4@gmail.com>
2023-10-24 07:14:10 -0500

    Forms

---
+++ CapaLogicaNegocio/EmpleadoLogica.cs
@@ -0,0 +1,50 @@
+﻿using CapaAccesoDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogicaNegocio
+{
+    public class EmpleadoLogica
+    {
+        private EmpleadoDatos empleadoDatos;
+
+        public EmpleadoLogica(string connectionString)
+        {
+            empleadoDatos = new EmpleadoDatos(connectionString);
+        }
+
+        public void CrearEmpleado(Empleado empleado)
+        {
+            // Puedes agregar lógica adicional aquí antes de llamar a la capa de acceso a datos.
+            empleadoDatos.CrearEmpleado(empleado);
+        }
+
+        public List<Empleado> LeerEmpleados()
+        {
+            // Puedes agregar lógica adicional aquí antes de llamar a la capa de acceso a datos.
+            return empleadoDatos.LeerEmpleados();
+        }
+
+        public Empleado LeerEmpleadoPorID(int idEmpleado)
+        {
+            // Puedes agregar lógica adicional aquí antes de llamar a la capa de acceso a datos.
+            return empleadoDatos.LeerEmpleadoPorID(idEmpleado);
+        }
+
+        public void ActualizarEmpleado(Empleado empleado)
+        {
+            // Puedes agregar lógica adicional aquí antes de llamar a la capa de acceso a datos.
+            empleadoDatos.ActualizarEmpleado(empleado);
+        }
+
+        public void EliminarEmpleado(int idEmpleado)
+        {
+            // Puedes agregar lógica adicional aquí antes de llamar a la capa de acceso a datos.
+            empleadoDatos.EliminarEmpleado(idEmpleado);
+        }
+    }
+}
